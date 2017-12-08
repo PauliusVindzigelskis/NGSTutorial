2f67907cb570a09d41fac840d4bc245c0e08b862
Paulius Vindzigelskis <p.vindzigelskis@gmail.com>
2017-12-08 17:55:51 -0600

    demo app fix

--- NGSTutorialControllerDemo/NGSTutorialControllerDemo/ViewController.m
+++ NGSTutorialControllerDemo/NGSTutorialControllerDemo/ViewController.m
@@ -9,21 +9,58 @@
 #import "ViewController.h"
 #import <NGSTutorial/NGSTutorial.h>
 
-@interface ViewController ()
+@interface ViewController () <NGSTutorialControllerDataSource>
+@property (weak, nonatomic) IBOutlet UIStackView *stackView;
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    self.view.backgroundColor = [UIColor yellowColor];
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    NGSTutorialController *controller = [[NGSTutorialController alloc] init];
+    controller.dataSource = self;
+    [controller showAnimated:YES];
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - NGSTutorialController Data Source
+
+-(NSInteger)numberOfScenesInTutorialController:(NGSTutorialController *)tutorial
+{
+    return 2;
+}
+
+-(NSInteger)tutorialController:(NGSTutorialController *)tutorial numberOfItemsInScene:(NSInteger)scene
+{
+    NSInteger count = [self.stackView arrangedSubviews].count / 2;
+    return count;
+}
+
+-(NGSItem *)tutorialController:(NGSTutorialController *)tutorial itemAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSArray<UIView*> *views = [self.stackView arrangedSubviews];
+    NSInteger itemsPerScene = [self.stackView arrangedSubviews].count / 2;
+    UIView *view = views[indexPath.item + (indexPath.section * itemsPerScene)];
+    
+    NSString *text = [NSString stringWithFormat:@"Item No.%lu", indexPath.item];
+    NSAttributedString *attrString = [[NSAttributedString alloc] initWithString:text attributes:@{
+                                                                                                  NSFontAttributeName : [UIFont systemFontOfSize:14.f]
+                                                                                                  }];
+    
+    NGSItem *item = [[NGSItem alloc] init];
+    item.target = view;
+    item.cornerRadius = 5.f;
+    item.text = attrString;
+    item.position = indexPath.section == 0 ? JMPositionRight : JMPositionLeft;
+    item.margin = 20.f;
+    
+    return item;
 }
 
 
